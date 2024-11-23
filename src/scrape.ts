34f8f2fa9cfe859fb1aceace3b85e66f3d31e3d0
kotarokomori <komopro.works@gmail.com>
2024-11-23 17:17:57 +0900

    途中

--- src/scrape.ts
+++ src/scrape.ts
@@ -1,8 +1,12 @@
 import { chromium } from "playwright";
+import env from "dotenv";
+// @ts-ignore
+import { GoogleSpreadsheet } from "google-spreadsheet";
+env.config();
 
 const browser = await chromium.launch();
 const page = await browser.newPage();
-await page.goto("https://kurukuru-dev.github.io/astro-ec-site/");
+await page.goto(process.env.TARGET_URL || "");
 
 interface Product {
   productName: string;
@@ -16,56 +20,75 @@ type ProductList = Product[];
  * 与えられた商品リスト名と商品数から3つの商品情報を取得
  * 3つの商品情報: 商品名、ブランド名、発売日
  */
-async function getProducts(productListName: string, productCount: number): Promise<ProductList> {
-  let productList = [];
-  const cardLocator = page.locator(`${productListName} >> li#card`);
+// async function getProducts(productListName: string, productCount: number): Promise<ProductList> {
+//   let productList = [];
+//   const cardLocator = page.locator(`${productListName} >> li#card`);
 
-  for (let i = 0; i < productCount; i++) {
-    await cardLocator.nth(i).click();
+//   for (let i = 0; i < productCount; i++) {
+//     await cardLocator.nth(i).click();
 
-    const productNameLocator = page.locator("#productName");
-    const productName: string = (await productNameLocator.textContent()) || "";
+//     const productNameLocator = page.locator("#productName");
+//     const productName: string = (await productNameLocator.textContent()) || "";
 
-    const brandNameLocator = page.locator("#brandName");
-    const brandName: string = (await brandNameLocator.textContent()) || "";
+//     const brandNameLocator = page.locator("#brandName");
+//     const brandName: string = (await brandNameLocator.textContent()) || "";
 
-    const releaseDateLocator = page.locator("#releaseDate");
-    const releaseDate: string = (await releaseDateLocator.textContent()) || "";
+//     const releaseDateLocator = page.locator("#releaseDate");
+//     const releaseDate: string = (await releaseDateLocator.textContent()) || "";
 
-    productList.push({
-      productName,
-      brandName,
-      releaseDate,
-    });
+//     productList.push({
+//       productName,
+//       brandName,
+//       releaseDate,
+//     });
 
-    const closeLocator = page.locator("#link");
-    await closeLocator.click();
-  }
+//     const closeLocator = page.locator("#link");
+//     await closeLocator.click();
+//   }
 
-  return productList;
-}
+//   return productList;
+// }
 
 // 検索欄で同じブランドの数を取得(本来は検索結果の商品数を取得したり、そのブランドの商品名を取得するなどの処理を行うが簡略化)
-async function getSameBrandCount(productName: string): Promise<number> {
-  const searchLocator = page.locator("#link:has-text('ブランド検索')");
-  await searchLocator.click();
+// async function getSameBrandCount(productName: string): Promise<number> {
+//   const searchLocator = page.locator("#link:has-text('ブランド検索')");
+//   await searchLocator.click();
 
-  const searchInoutLocator = page.locator("#searchInput");
-  await searchInoutLocator.fill(productName);
-  await page.waitForTimeout(2000); // ここの処理いけてない
-  const brandLocator1 = page.locator("li#brand");
+//   const searchInoutLocator = page.locator("#searchInput");
+//   await searchInoutLocator.fill(productName);
+//   await page.waitForTimeout(2000); // ここの処理いけてない
+//   const brandLocator1 = page.locator("li#brand");
 
-  const selectLocator = page.locator("#select");
-  await selectLocator.selectOption({ label: productName });
-  await page.waitForTimeout(2000); // ここの処理いけてない
-  const brandLocator2 = page.locator("li#brand");
+//   const selectLocator = page.locator("#select");
+//   await selectLocator.selectOption({ label: productName });
+//   await page.waitForTimeout(2000); // ここの処理いけてない
+//   const brandLocator2 = page.locator("li#brand");
 
-  // 触ってみたかったから、謎の処理を追加
-  const isCountMatch = await brandLocator1.count() === await brandLocator2.count();
-  return isCountMatch ? await brandLocator1.count(): 0;
+//   // 触ってみたかったから、謎の処理を追加
+//   const isCountMatch = await brandLocator1.count() === await brandLocator2.count();
+//   return isCountMatch ? await brandLocator1.count(): 0;
+// }
+
+async function writeGoogleSpreadSheet() {
+  const doc = new GoogleSpreadsheet(process.env.GOOGLE_SPREADSHEET_ID || "");
+
+  doc.useServiceAccountAuth({
+    client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL || "",
+    private_key: process.env.GOOGLE_PRIVATE_KEY || "",
+  })
+
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const title = `${year}年${month}月${day}日の商品情報`;
+
+  await doc.addSheet({ title, headerValues: ["商品名", "ブランド名", "発売日"] });
+
 }
 
-const newProducts = await getProducts("#newProducts", 5);
-const popularProducts = await getProducts("#popularProducts", 5);
-const sameBrandCount = await getSameBrandCount("ブランドA");
+// const newProducts = await getProducts("#newProducts", 5);
+// const popularProducts = await getProducts("#popularProducts", 5);
+// const sameBrandCount = await getSameBrandCount("ブランドA");
+await writeGoogleSpreadSheet()
 await browser.close();
